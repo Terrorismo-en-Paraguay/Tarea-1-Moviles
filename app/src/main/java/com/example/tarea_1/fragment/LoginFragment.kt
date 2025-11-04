e1385f9876c1faf9bcb7bd882c467e4fdc379eb6
Samue <samuelsanchezsandoval1808@gmail.com>
2025-11-04 20:50:57 +0100

    usuario y contrasenia

--- app/src/main/java/com/example/tarea_1/fragment/LoginFragment.kt
+++ app/src/main/java/com/example/tarea_1/fragment/LoginFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.tarea_1.databinding.FragmentLoginBinding
+import com.google.android.material.snackbar.Snackbar
 
 class LoginFragment : Fragment() {
     private lateinit var binding: FragmentLoginBinding
@@ -22,5 +24,26 @@ class LoginFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentLoginBinding.inflate(inflater,container,false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?){
+
+        super.onViewCreated(view, savedInstanceState)
+
+        if (binding.nombreUsuarioContenido.text.toString().isEmpty()){
+
+        }
+
+        binding.iniciarSesion.setOnClickListener {
+            if ((binding.nombreUsuarioContenido.text.toString() == "admin") && binding.contraseniaContenido.text.toString() == "1234")
+            {
+                val snackbar = Snackbar.make(binding.root, "Login correcto",Snackbar.LENGTH_LONG)
+                snackbar.show()
+            }else{
+                val snackbar = Snackbar.make(binding.root, "Login incorrecto",Snackbar.LENGTH_LONG)
+                snackbar.show()
+            }
+        }
+
     }
 }
