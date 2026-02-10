c361deca4ecc19c9dedb0052191bd4fb52bc229f
Terrorismo-en-Paraguay <5519473@alu.murciaeduca.es>
2026-02-10 23:14:58 +0100

    firebase inicio sesion?

--- app/src/main/java/com/example/tarea_1/fragment/LoginFragment.kt
+++ app/src/main/java/com/example/tarea_1/fragment/LoginFragment.kt
@@ -9,19 +9,25 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.example.tarea_1.databinding.FragmentLoginBinding
 import com.google.android.material.snackbar.Snackbar
 import com.example.tarea_1.R
+import com.example.tarea_1.ui.UserUIState
 import com.example.tarea_1.viewmodels.LoginViewModelFactory
 import com.example.tarea_1.viewmodels.Loginviewmodel
+import kotlinx.coroutines.launch
 
 class LoginFragment : Fragment() {
     private lateinit var binding: FragmentLoginBinding
 
-    private val loginviewmodel: Loginviewmodel by authviewModels { LoginViewModelFactory }
+    private val loginviewmodel: Loginviewmodel by viewModels { LoginViewModelFactory() }
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
@@ -41,7 +47,6 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?){
         super.onViewCreated(view, savedInstanceState)
-        val loginviewmodel = ViewModelProvider(this)[Loginviewmodel::class.java]
         loginviewmodel.isButtonEnabled.observe(viewLifecycleOwner, object: Observer<Boolean> {
             override fun onChanged(value: Boolean) {
                 binding.iniciarSesion.isEnabled = value
@@ -51,14 +56,29 @@ class LoginFragment : Fragment() {
         binding.contraseniaContenido.addTextChangedListener{loginviewmodel.onTextpassword(it.toString())}
 
         binding.iniciarSesion.setOnClickListener {
-            if ((binding.nombreUsuarioContenido.text.toString() == "admin") && binding.contraseniaContenido.text.toString() == "1234")
-            {
-                val snackbar = Snackbar.make(binding.root, "Login correcto",Snackbar.LENGTH_LONG)
-                snackbar.show()
-                findNavController().navigate(R.id.login_to_list)
-            }else{
-                val snackbar = Snackbar.make(binding.root, "Login incorrecto",Snackbar.LENGTH_LONG)
-                snackbar.show()
+            loginviewmodel.iniciarSesion(binding.nombreUsuarioContenido.text.toString(),binding.contraseniaContenido.text.toString())
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                loginviewmodel.userUIState.collect { state ->
+                    when(state) {
+                        is UserUIState.Loading -> {
+                            binding.iniciarSesion.visibility = View.VISIBLE
+                        }
+                        is UserUIState.Success -> {
+                            binding.iniciarSesion.visibility = View.GONE
+                            findNavController().navigate(R.id.login_to_list)
+                        }
+                        is UserUIState.Error -> {
+                            binding.iniciarSesion.visibility = View.GONE
+                            Snackbar.make(view, state.message, Snackbar.LENGTH_LONG).show()
+                        }
+                        is  UserUIState.Idle -> {
+                            binding.iniciarSesion.visibility = View.GONE
+                        }
+                    }
+                }
             }
         }
 
